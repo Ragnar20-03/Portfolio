57ce9a3788338dea1f366fc294e03d236a6e7d2a
roshanpp20@gmail.com <roshanpatil@Ragnar20-03.local>
2024-01-04 01:20:33 +0530

    Server Done

--- Server/server.js
+++ Server/server.js
@@ -6,8 +6,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const admin = require('./Routes/admin.js')
+const user = require('./Routes/user.js')
 
 app.use('/admin' , admin)
+app.use('/user' , user)
 
 app.listen(5100 , ()=>{
     console.log("Server Started on port Number : 5100");
